e525b78e4e832d906465939d7408c266eb3a45aa
Dieter Prado <dayterp@gmail.com>
2021-06-28 18:36:03 -0500

    Clase 14, getters y setters

--- src/Main.java
+++ src/Main.java
@@ -11,12 +11,11 @@ public class Main {
         System.out.println(myDoctor.speciality);
 
         Patient patient = new Patient("Alejandra", "alejandra@mail.com");
-        System.out.println(patient.name);
-        System.out.println(patient.email);
-        patient.weight = 60.5;
-        patient.height = 1.65;
-        System.out.println(patient.weight);
-        System.out.println(patient.height);
+        patient.setWeight(54.6);
+        System.out.println(patient.getWeight());
+
+        patient.setPhoneNumber("12345678");
+        System.out.println(patient.getPhoneNumber());
 
 
 
--- src/Patient.java
+++ src/Patient.java
@@ -13,7 +13,33 @@ public class Patient {
         this.name = name;
         this.email = email;
         this.weight = 54.5;
+    }
 
+    public void setWeight(double weight){
+        this.weight = weight;
+    }
 
+    public String getWeight(){
+        return this.weight + " Kg.";
+    }
+
+    public void setHeight(double height){
+        this.height = height;
+    }
+
+    public String getHeight(){
+        return this.height + " Mts.";
+    }
+
+    public void setPhoneNumber(String phoneNumber){
+        if (phoneNumber.length() != 8){
+            System.out.println("El número telefónico debe de tener 8 digitos");
+        }else{
+            this.phoneNumber = phoneNumber;
+        }
+    }
+
+    public String getPhoneNumber(){
+        return this.phoneNumber;
     }
 }
